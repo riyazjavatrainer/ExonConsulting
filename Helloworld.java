9d9b683acd67e6775f8c07dbbf131d71740bd8db
Riyaz <riyaz.javatrainer@gmail.com>
2016-06-30 10:39:37 +0530

    Helloword
    sample program

---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Helloworld{
+	 
+	 void display(){
+		System.out.println("Parent class display method");
+	}
+	 
+	public static void main(String[] args) {
+		 List list = new ArrayList<>();
+		 Map map = new HashMap();
+	}
+
+}
